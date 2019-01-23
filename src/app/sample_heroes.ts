7e93ee49a37465e0037544ab084d53192888dcb3
aaxzheng <aaronzheng3@gmail.com>
2019-01-23 14:39:22 -0500

    made an interactive ul element

---
+++ src/app/sample_heroes.ts
@@ -0,0 +1,11 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 2, name: "Arm-Fall-off Lad"},
+  {id: 3, name: "Kite-man"},
+  {id: 4, name: "Mr. Furious"},
+  {id: 5, name: "The Shoveler"},
+  {id: 6, name: "The Bowler"},
+  {id: 7, name: "Invisible Boy"},
+  {id: 8, name: "Captain Amazing"}
+]
